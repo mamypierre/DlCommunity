4193cc0407f7ea9f1f7820e69b0d5d094c01d5bc
Jean-PatrickLC <jp.lepresle.chhun@hotmail.fr>
2018-07-11 08:28:45 +0200

    forum

--- src/DlCommunity/ForumBundle/Controller/DefaultController.php
+++ src/DlCommunity/ForumBundle/Controller/DefaultController.php
@@ -8,6 +8,23 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('@DlCommunityForum/Default/index.html.twig');
+
+        $repository = $this
+            ->getDoctrine()
+            ->getManager() 
+            ->getRepository('DlCommunityCoreBundle:User_type');
+
+        
+        $listPseudo = $repository->findAll();
+        
+        print_r($listPseudo);
+
+       /* foreach ($listPseudo as $pseudo) {
+
+            echo $pseudo->getContent();
+
+        }*/
+
+        return $this->render('@DlCommunityForum/Default/index.html.twig',array('tet'=>'mitoooooooo'));
     }
 }
